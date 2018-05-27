d521dcee41ab2105709fd255fce6d813fdbe7747
hjpotter92 <hjpotter92+github@gmail.com>
2018-05-27 12:32:07 +0530

    Move `dired+` to `use-package`
     * Start `server-mode` if it is a GUI client
     * Setup `magit`, `focus`, `company`, `ace-jump` packages
    
    Signed-off-by: hjpotter92 <hjpotter92+github@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,6 @@
 ;;; init --- Summary
 ;;; Commentary:
+
 ;; You might already have this line
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -36,24 +37,15 @@
    (quote
     ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(debug-on-error t)
- '(display-time-format "%a %d %b, %I:%M %p")
- '(display-time-mode t)
- '(dynamic-completion-mode t)
  '(global-git-commit-mode t)
  '(global-highlight-parentheses-mode t)
- '(global-visual-line-mode t)
- '(inhibit-startup-screen t)
- '(initial-scratch-message nil)
- '(line-number-mode t)
  '(package-selected-packages
    (quote
-    (all-the-icons-ivy all-the-icons-dired magit diminish company-jedi ac-html-bootstrap company-ngram company-quickhelp company-shell company-statistics unicode-fonts counsel-etags counsel-gtags ctags-update region-bindings-mode multiple-cursors web git-commit git-gutter git-gutter+ git-gutter-fringe git-ps1-mode gitattributes-mode gitconfig gitconfig-mode company-emoji company-erlang company-go company-lua company-math company-rtags company-web editorconfig font-utils fontawesome format-all format-sql nose pipenv pydoc python-mode pythonic flycheck-color-mode-line flycheck-css-colorguard flycheck-elixir flycheck-mix flycheck-pycheckers flycheck-tcl xpm web-mode use-package-ensure-system-package use-package-el-get switch-window smartparens smart-window smart-mode-line smart-cursor-color monokai-theme mode-icons markdown-preview-mode markdown-mode+ image-dired+ highlight-parentheses flycheck emmet-mode counsel company auto-minor-mode auto-indent-mode auto-auto-indent)))
- '(save-place t nil (saveplace))
+    (minibuffer-line flycheck-package package-lint ace-jump-mode company-flx focus graphviz-dot-mode all-the-icons-ivy all-the-icons-dired magit diminish company-jedi ac-html-bootstrap company-ngram company-quickhelp company-shell company-statistics unicode-fonts counsel-etags counsel-gtags ctags-update region-bindings-mode multiple-cursors web git-commit git-gutter git-gutter+ git-gutter-fringe git-ps1-mode gitattributes-mode gitconfig gitconfig-mode company-emoji company-erlang company-go company-lua company-math company-rtags company-web editorconfig font-utils fontawesome format-all format-sql nose pipenv pydoc python-mode flycheck-color-mode-line flycheck-css-colorguard flycheck-elixir flycheck-mix flycheck-pycheckers flycheck-tcl xpm web-mode use-package-ensure-system-package use-package-el-get switch-window smartparens smart-window smart-mode-line smart-cursor-color monokai-theme mode-icons markdown-preview-mode markdown-mode+ image-dired+ highlight-parentheses flycheck emmet-mode counsel company auto-minor-mode auto-indent-mode auto-auto-indent)))
+ '(save-place-mode t nil (saveplace))
  '(send-mail-function (quote smtpmail-send-it))
  '(show-smartparens-global-mode t)
  '(smartparens-global-mode t)
- '(smtpmail-smtp-server "email-smtp.us-west-2.amazonaws.com")
- '(smtpmail-smtp-service 587)
  '(starttls-gnutls-program "gnutls-cli")
  '(starttls-use-gnutls t)
  '(tool-bar-mode nil)
@@ -68,25 +60,38 @@
 
 (defvar highlight-blocks-mode)
 (defvar sml/replacer-regexp-list)
+(defvar use-package-minimum-reported-time)
+(defvar use-package-compute-statistics)
+(defvar company-dabbrev-downcase)
+(defvar region-bindings-mode-map)
 
 (setq-default abbrev-mode t)
 (setq-default auto-revert-mode t)
 (setq-default buffer-file-coding-system 'utf-8-unix)
-(setq-default indent-tabs-mode nil)
 
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(setq x-stretch-cursor t)
-(setq require-final-newline t)
-(setq frame-title-format '("[ %b ] @ Emacs " emacs-version))
 (setq highlight-blocks-mode t)
 
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 
-(require 'dired+)
+;; (require 'dired+)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+(progn
+  (setq use-package-minimum-reported-time 0)
+  (setq use-package-compute-statistics t)
+  (require 'use-package))
+
+(use-package dired+
+  :commands toggle-diredp-find-file-reuse-dir)
+
+(use-package dired
+  :commands (dired
+             find-name-dired
+             find-dired)
+  :config
+  (toggle-diredp-find-file-reuse-dir 1))
 
 (use-package smart-window)
 
@@ -101,9 +106,26 @@
   :ensure t
   :config (image-diredx-async-mode 1))
 
+(use-package magit
+  :commands magit-get-top-dir
+  :bind (("C-c g" . magit-status)
+         ("C-c C-g l" . magit-file-log))
+  :init
+  ;; we no longer need vc-git
+  (delete 'Git vc-handled-backends))
+
+(use-package focus
+  :ensure t
+  :bind ("C-c C-f" . focus-mode))
+
 (use-package flycheck
   :ensure t
-  :config (global-flycheck-mode))
+  :init (global-flycheck-mode))
+
+(use-package flycheck-package
+  :commands flycheck-package-setup
+  :after (flycheck)
+  :init (flycheck-package-setup))
 
 (use-package ivy
   :ensure t
@@ -128,7 +150,6 @@
   ("<f1> v" . counsel-describe-variable)
   ("<f1> b" . counsel-descbinds)
   ("C-c k" . counsel-rg)
-  ("C-c g" . counsel-git)
   ("M-y" . counsel-yank-pop))
 
 (use-package all-the-icons-ivy
@@ -153,7 +174,7 @@
    ("\\.mustache\\'" . web-mode)
    ("\\.djhtml\\'" . web-mode))
   :config
-  (setq 'web-mode-markup-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
   (setq web-mode-enable-current-element-highlight t)
@@ -212,24 +233,46 @@
   (with-eval-after-load 'company
     '(push 'company-jedi company-backends)))
 
+(use-package company-flx
+  :ensure t
+  :after (company)
+  :config
+  (with-eval-after-load 'company
+    (company-flx-mode +1)))
+
 (use-package company-statistics
   :ensure t)
+
+(use-package company-quickhelp
+  :ensure t
+  :config
+  (progn
+    (setq company-quickhelp-delay 0.1)
+    (company-quickhelp-mode)))
 
 (use-package company
   :ensure t
   :diminish company-mode
   :requires company-statistics
   :config
-  (defun my/python-mode-hook ()
-    "Add jedi backend to company"
-    (with-eval-after-load 'company
-      '(push 'company-jedi company-backends)))
-  (add-hook 'python-mode-hook 'my/python-mode-hook)
+  (progn
+    (defun my/python-mode-hook ()
+      "Add jedi backend to company"
+      (with-eval-after-load 'company
+        '(push 'company-jedi company-backends)))
+    (add-hook 'python-mode-hook 'my/python-mode-hook)
+    (setq company-dabbrev-downcase 0)
+    (setq company-idle-delay 0))
   :hook
   ((after-init . global-company-mode)
    (after-init . company-statistics-mode)))
 
+(use-package ace-jump-mode
+  :bind
+  (("C-." . ace-jump-mode)))
+
 (use-package monokai-theme
+  :if (display-graphic-p)
   :config
   (progn
     (load-theme 'monokai t)))
@@ -238,6 +281,17 @@
   :ensure t
   :after (smart-mode-line)
   :config (mode-icons-mode))
+
+(use-package minibuffer-line
+  :if (display-graphic-p)
+  :ensure t
+  :defer 1
+  :init
+  (progn
+    (setq-default display-time-format "")
+    (setq minibuffer-line-format (format-time-string "%l:%M %b %d %a")))
+  :config
+  (minibuffer-line-mode))
 
 (use-package smart-mode-line
   :ensure t
@@ -258,16 +312,46 @@
     (diminish 'abbrev-mode "Ab")
     (diminish 'outline-mode)))
 
+(use-package server
+  :if (display-graphic-p)
+  :config
+  (progn
+    (or (eq (server-running-p) t)
+        (server-start))))
+
 (use-package emacs
   :ensure t
   :diminish
   (visual-line-mode abbrev-mode)
+  :bind
+  (("C-c m" . menu-bar-mode)
+   ("C-c s" . scroll-bar-mode)
+   ("RET" . newline-and-indent))
   :init
   (progn
     (setq package-archive-priorities
           '(("melpa" . 20)
             ("marmalade" . 15)
-            ("gnu" . 10)))))
+            ("gnu" . 10)))
+    (setq frame-title-format
+          '("["
+            (:eval (if (buffer-file-name)
+                       (abbreviate-file-name (buffer-file-name))
+                     "%b"))
+            " (%*%+%z) %F@" emacs-version "]"))
+    (setq x-stretch-cursor t)
+    (setq-default display-time-default-load-average nil)
+    (setq-default display-time-format "%a %d %b, %I:%M %p")
+    (setq-default inhibit-startup-screen t)
+    (setq-default initial-scratch-message nil)
+    (setq-default indent-tabs-mode nil)
+    (setq require-final-newline t))
+  :config
+  (progn
+    (global-visual-line-mode t)
+    (line-number-mode t)
+    (defalias 'yes-or-no-p 'y-or-n-p)
+    (display-time-mode t)))
 
 ;; (use-package virtualenvwrapper
 ;;   :init
