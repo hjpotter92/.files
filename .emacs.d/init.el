124a484ed31d97d8c9547e26d38e59ada9d39690
hjpotter92 <hjpotter92+github@gmail.com>
2018-05-29 10:02:55 +0530

    Revert `Mod+Tab` behaviour in i3.
    Use fuzzy matching for rofi
    
    Signed-off-by: hjpotter92 <hjpotter92+github@gmail.com>

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,7 @@
 ;;; init --- Summary
 ;;; Commentary:
 
+;;; Code
 ;; You might already have this line
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -57,7 +58,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 85 :width normal :foundry "unknown" :family "Hack Nerd Font")))))
+ '(default ((t
+             (:inherit nil :stipple nil
+                       :background "#272822" :foreground "#F8F8F2"
+                       :inverse-video nil :box nil :strike-through nil
+                       :overline nil :underline nil :slant normal :weight normal
+                       :height 85 :width normal :foundry "unknown"
+                       :family "Hack Nerd Font")))))
 
 (defvar highlight-blocks-mode)
 (defvar sml/replacer-regexp-list)
